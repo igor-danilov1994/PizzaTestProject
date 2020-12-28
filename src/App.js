fdbff839d0edf6470d906c9ff05266afffece3fa
igor-danilov1994 <igor.danilov1824@gmail.com>
2020-12-28 10:33:29 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Header from './Component/Header/Header'
+import Content from './Component/Content/Content'
+
+function App() {
+    return (
+        <div class="wrapper">
+            <Header />
+            <Content />
+        </div>
+    );
+}
+
+export default App;
